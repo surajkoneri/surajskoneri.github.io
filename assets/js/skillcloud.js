6ca694e9d16c6aac7e52208302b457543cbd080d
Suraj Koneri <suraj.koneri@bpx.com>
2022-05-22 09:40:31 -0500

    fixed contact me

---
+++ assets/js/skillcloud.js
@@ -0,0 +1,294 @@
+zingchart.render({ 
+    id: 'myskillCloud',    
+    data: {      
+        type: 'wordcloud',      
+        options: {
+            words: [        
+            {
+                 text:".Net",
+                 count:5
+             },
+             {
+                 text:"C#",
+                 count:5
+             },
+             {
+                 text:"Azure",
+                 count:9
+             },
+             {
+                 text:"AWS",
+                 count:7
+             },
+             {
+                 text:"azfunctionapp",
+                 count:8
+             },
+             {
+                 text:"Api management",
+                 count:7
+             },
+             {
+                 text:"VNET/Subnet",
+                 count:8
+             },
+             {
+                 text:"API Gateway",
+                 count:6
+             },
+             {
+                 text:"AzLoad Balancer",
+                 count:7
+             },
+             {
+                 text:"ASP.NET",
+                 count:5
+             },
+             {
+                 text:"Python",
+                 count:7
+             },
+             {
+                 text:"Powershell",
+                 count:8
+             },
+             {
+                 text:"AzCLI",
+                 count:7
+             },
+             {
+                 text:"boto3",
+                 count:6
+             },
+             {
+                 text:"Lambda",
+                 count:8
+             },
+             {
+                 text:"CloudFormation",
+                 count:6
+             },
+             {
+                 text:"CfCT",
+                 count:6
+             },
+             {
+                 text:"Azure DevOps",
+                 count:7
+             },
+             {
+                 text:"ADO CI/CD Pipelines",
+                 count:8
+             },
+             {
+                 text:"SAML",
+                 count:8
+             },
+             {
+                 text:"AzureAD",
+                 count:7
+             },
+             {
+                 text:"SPNs",
+                 count:6
+             },
+             {
+                 text:"Stonebranch",
+                 count:8
+             },
+             {
+                 text:"Azure Data Factory",
+                 count:9
+             },
+             {
+                 text:"EventHub",
+                 count:8
+             },
+             {
+                 text:"Azure Table Storage",
+                 count:8
+             },
+             {
+                 text:"Azure CosmosDB",
+                 count:4
+             },
+             {
+                 text:"Azure ServiceBus",
+                 count:5
+             },
+             {
+                 text:"Palo Alto Firewall",
+                 count:7
+             },
+             {
+                 text:"PanOS",
+                 count:6
+             },
+             {
+                 text:"ARM Templates",
+                 count:6
+             },
+             {
+                 text:"YAML",
+                 count:8
+             },
+             {
+                 text:"Ansible Tower",
+                 count:8
+             },
+             {
+                 text:"Ansible PlayBook",
+                 count:6
+             },
+             {
+                 text:"Active Directory",
+                 count:8
+             },
+             {
+                 text:"Windows10",
+                 count:8
+             },
+             {
+                 text:"Widnows Server 2019",
+                 count:6
+             },
+             {
+                 text:"IIS",
+                 count:8
+             },
+             {
+                 text:"Tomcat",
+                 count:7
+             },
+             {
+                 text:"Dynatrace",
+                 count:6
+             },
+             {
+                 text:"Azure WebApps",
+                 count:6
+             },
+             {
+                 text:"Serverless Framework",
+                 count:6
+             },
+             {
+                 text:"SnowFlake",
+                 count:5
+             },
+             {
+                 text:"ServiceNOW",
+                 count:6
+             },
+             {
+                 text:"Automation",
+                 count:9
+             },
+             {
+                 text:"LogicApps",
+                 count:7
+             },
+             {
+                 text:"PowerPlatform",
+                 count:6
+             },
+             {
+                 text:"Infrastructure as Code",
+                 count:7
+             },
+             {
+                 text:"Configuration as Code",
+                 count:7
+             },
+             {
+                 text:"Office365",
+                 count:5
+             },
+             {
+                 text:"Azure Storage Explorer",
+                 count:4
+             },
+             {
+                 text:"VS Code",
+                 count:8
+             },
+             {
+                 text:"Visual Studio 2019",
+                 count:5
+             },
+             {
+                 text:"Git",
+                 count:8
+             },
+             {
+                 text:"JScape",
+                 count:8
+             },
+             {
+                 text:"XML",
+                 count:4
+             },
+             {
+                 text:"GIS",
+                 count:7
+             },
+             {
+                 text:"ArcGIS",
+                 count:6
+             },
+             {
+                 text:"HTML",
+                 count:5
+             },
+             {
+                 text:"JavaScript",
+                 count:4
+             },
+             {
+                 text:"Solution Designing",
+                 count:9
+             },
+             {
+                 text:"ITIL",
+                 count:9
+             },
+             {
+                 text:"Bot",
+                 count:5
+             },
+             {
+                 text:"BOT Framework",
+                 count:6
+             },
+             {
+                 text:"QnA Maker",
+                 count:7
+             },
+             {
+                 text:"REST APIs",
+                 count:6
+             },
+             {
+                 text:"Cognitive Services",
+                 count:6
+             },
+             {
+                 text:"LUIS",
+                 count:7
+             },
+             {
+                 text:"Network Design",
+                 count:7
+             },
+             {
+                 text:"VPC",
+                 count:5
+             },
+             {
+                 text:"Private EndPoints",
+                 count:7
+             }
+            ]    
+         }
+     } 
+     });
